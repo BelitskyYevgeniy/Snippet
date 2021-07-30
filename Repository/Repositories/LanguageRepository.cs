43174ae39cce6397d1e44a64595df763cd1c0f21
BelitskyYevgeniy <78808088+BelitskyYevgeniy@users.noreply.github.com>
2021-07-30 23:48:28 +0300

    Add LanguageRepository and ILanguageRepositoryAsync

---
+++ Repository/Repositories/LanguageRepository.cs
@@ -0,0 +1,17 @@
+﻿using Snippet.Data.Context;
+using Snippet.Data.Entities;
+using Snippet.Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snippet.Data.Repositories
+{
+    class LanguageRepository: GenericRepository<LanguageEntity>, ILanguageRepositoryAsync
+    {
+        public LanguageRepository(RepositoryContext db): base(db)
+        {
+
+        }
+    }
+}
