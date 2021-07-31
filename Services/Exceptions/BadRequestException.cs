5bba4283161900981db449b0a4f29effdfeb1768
German <geras210e@gmail.com>
2021-07-31 17:04:22 +0300

    Add badRequestException

---
+++ Services/Exceptions/BadRequestException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace Snippet.BLL.Exceptions
+{
+    public class BadRequestException : Exception 
+    {
+        public BadRequestException(string message):base(message)
+        {
+           
+        }
+    }
+}
