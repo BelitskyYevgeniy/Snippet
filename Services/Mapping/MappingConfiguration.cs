deede4793ac6dce374f91f01ccebc075eeec2331
German <geras210e@gmail.com>
2021-07-31 15:16:38 +0300

    add maping configuration

---
+++ Services/Mapping/MappingConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Repository.Entities;
+using Snippet.BLL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snippet.BLL.Mapping
+{
+    public class MappingConfiguration:Profile
+    {
+        public MappingConfiguration()
+        {
+            CreateMap<CommentEntity, Comment>().ReverseMap();
+            CreateMap<LanguageEntity, Language>().ReverseMap();
+            CreateMap<LikeEntity, Like>().ReverseMap();
+            CreateMap<PostEntity, Post>().ReverseMap();
+            CreateMap<TagEntity, Tag>().ReverseMap();
+            CreateMap<UserEntity, User>().ReverseMap();
+        }
+    }
+}
