4d63b540a28f1efac78c11c56a6f3000a26a2c6a
BelitskyYevgeniy <78808088+BelitskyYevgeniy@users.noreply.github.com>
2021-08-02 01:31:41 +0300

    Made not fully PostEntityFilterByUsers

---
+++ Repository/Filters/PostEntityFilters/PostFilterByUsers.cs
@@ -0,0 +1,21 @@
+﻿using Snippet.Data.Entities;
+using Snippet.Data.Interfaces.Filters;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snippet.Data.Filters.PostEntityFilters
+{
+    class PostFilterByUsers : IFilter<PostEntity>
+    {
+        public IEnumerable<UserEntity> Include { get; private set; }
+        public IEnumerable<UserEntity> Exclude { get; private set; }
+        public PostFilterByUsers(IEnumerable<UserEntity> include, IEnumerable<UserEntity> exclude)
+        {
+
+        }
+        public Predicate<PostEntity> Predicate => throw new NotImplementedException();
+
+        public int Degree { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
