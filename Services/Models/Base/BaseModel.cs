62bceec2ec801a8f77e68cdece8ec58fd29a837e
German <geras210e@gmail.com>
2021-08-01 12:32:33 +0300

    Add BaseModel

---
+++ Services/Models/Base/BaseModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Services.Models.Base
+{
+    public class BaseModel
+    {
+       [Key]
+       public int Id { get; set; }
+       
+    }
+}
