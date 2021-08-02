273e4afda1da4d6b937e6d2825dfbcabb3939497
German <geras210e@gmail.com>
2021-08-02 21:19:11 +0300

    Add TagController

---
+++ Snippet/Controllers/TagController.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Services.Models;
+using Snippet.BLL.Interfaces.Providers;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Snippet.WebAPI.Controllers
+{
+    public class TagController
+    {
+        private readonly IMapper _mapper;
+        private readonly ITagProvider _tagProvider;
+
+        public TagController(IMapper mapper,ITagProvider tagProvider)
+        {
+            _mapper = mapper;
+            _tagProvider = tagProvider;
+        }
+
+        public Task<bool> Delete(int id,CancellationToken ct)
+        {
+            return _tagProvider.DeleteAsync(id, ct);
+        }
+
+        public Task<IReadOnlyCollection<Tag>> Create(IReadOnlyCollection<Tag> tags,CancellationToken ct)
+        {
+            return _tagProvider.MakeAsync(tags, ct);
+        }
+
+        public Task<Tag> Update(Tag tag,CancellationToken ct)
+        {
+            return _tagProvider.UpdateAsync(tag, ct);
+        }
+    }
+}
