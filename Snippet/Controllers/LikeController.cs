b3e2fe0bcef9dc72cf9c3fe3b35f96a4ea239f3c
German <geras210e@gmail.com>
2021-08-02 21:55:43 +0300

    Add LikeController

---
+++ Snippet/Controllers/LikeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Interfaces.Providers;
+using Services.Models;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Snippet.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LikeController: ControllerBase
+    {
+        private readonly ILikeProvider _likeProvider;
+
+        public LikeController(ILikeProvider likeProvider)
+        {
+            _likeProvider = likeProvider;
+        }
+
+        [HttpPost]
+        public Task<Like> Like(Like like,CancellationToken ct)
+        {
+            return _likeProvider.Like(like,ct);
+        }
+    }
+}
