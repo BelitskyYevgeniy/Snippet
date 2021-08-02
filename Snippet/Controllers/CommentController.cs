8f661801efbe3c357a88e0da2e2b0564cdf77bc6
German <geras210e@gmail.com>
2021-08-02 16:10:04 +0300

    Add Comment controller

---
+++ Snippet/Controllers/CommentController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Interfaces.Providers;
+using Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Snippet.WebAPI.Controllers
+{
+    public class CommentController:ControllerBase
+    {
+        private readonly ICommentProvider _commentProvider;
+
+        public CommentController(ICommentProvider commentProvider)
+        {
+            _commentProvider = commentProvider;
+        }
+        [HttpGet]
+        public Task<IReadOnlyCollection<Comment>> GetAllByPostId(int postId,CancellationToken ct)
+        {
+            return _commentProvider.GetAllByPostIdAsync(postId,ct);
+        }
+
+        [HttpPost]
+        public Task<Comment> Create(Comment comment,CancellationToken ct)
+        {
+            return _commentProvider.MakeAsync(comment, ct);
+        }
+
+        [HttpDelete]
+        public Task<bool> Delete(int id,CancellationToken ct)
+        {
+            return _commentProvider.DeleteAsync(id, ct);
+        }
+
+        public Task<Comment> Update(Comment comment,CancellationToken ct)
+        {
+            
+            return _commentProvider.UpdateAsync(comment, ct);
+        }
+
+        public Task<Comment> ToAnswer(Comment comment,int FatherCommentId,CancellationToken ct)
+        {
+            comment.FatherCommentId = FatherCommentId;
+            return _commentProvider.MakeAsync(comment, ct);
+        }
+ 
+    }
+}
--- Snippet/Controllers/PostController.cs
+++ Snippet/Controllers/PostController.cs
@@ -3,10 +3,7 @@ using Services.Interfaces.Providers;
 using Services.Interfaces.Services;
 using Services.Models;
 using Services.Responses;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
