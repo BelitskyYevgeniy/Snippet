d0e403673bd1a16048cf1997652317153b8fd452
German <geras210e@gmail.com>
2021-08-03 20:00:46 +0300

    Add UserController

---
+++ Snippet/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Interfaces.Providers;
+using Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Snippet.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController :ControllerBase
+    {
+        private readonly IUserProvider _userProvider;
+
+        public UserController(IUserProvider userProvider)
+        {
+            _userProvider = userProvider;
+        }
+
+        [HttpGet("{id:int}")]
+        public Task<User> GetById(int id, CancellationToken ct)
+        {
+            return _userProvider.GetById(id, ct);
+        }
+    }
+}
