3df344476d13cff151d78a27d06679e989e01952
German <geras210e@gmail.com>
2021-08-03 20:37:13 +0300

    Add Swagger

--- Snippet/Startup.cs
+++ Snippet/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using Services.Configuration;
 using Snippet.Data.Configuration;
 using Snippet.WebAPI.Middleware;
@@ -24,6 +25,10 @@ namespace Snippet.WebAPI
             services.AddControllers();
             services.RegisterProviders();
             services.AddRepository(Configuration.GetConnectionString("DefaultConnection"));
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Snippet.WebAPI", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,7 +46,9 @@ namespace Snippet.WebAPI
             app.UseAuthorization();
             app.UseCancellationTokenTimeout();
             app.UseExceptionHandler();
-            
+
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Snippet.WebAPI v1"));
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
