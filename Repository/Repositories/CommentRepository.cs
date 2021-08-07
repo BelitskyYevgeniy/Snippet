ebf64ecf223e96e1e26e35fe466f22662bffaaf9
German <geras210e@gmail.com>
2021-08-07 18:24:23 +0300

    add method for remove posts

--- Repository/Repositories/CommentRepository.cs
+++ Repository/Repositories/CommentRepository.cs
@@ -1,11 +1,27 @@
 ﻿using Snippet.Data.Context;
 using Snippet.Data.Entities;
 using Snippet.Data.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Snippet.Data.Repositories
 {
     public class CommentRepository: GenericRepository<CommentEntity>, ICommentRepositoryAsync
     {
         public CommentRepository(RepositoryContext db) : base(db) { }
+
+        public async Task RemoveAllByPostId(int postId, CancellationToken ct = default)
+        {
+            var comments = await FindAsync(0, await GetCount(ct),
+            new List<Expression<Func<CommentEntity, bool>>>()
+            {
+                comment => comment.PostId == postId
+            }, null, null, ct);
+
+            _dbContext.RemoveRange(comments);
+        }
     }
 }
