444f3fc6b24c963adbbf9229c0bf66adbf3c10dd
German <geras210e@gmail.com>
2021-08-09 21:39:17 +0300

    add PaginationService

---
+++ Services/Services/PaginationService.cs
@@ -0,0 +1,30 @@
+﻿using Services.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Services
+{
+    public class PaginationService : IPaginationService
+    {
+        private  readonly int maxCount = 50;
+
+        public int ValidateCount(int count)
+        {
+            if (count > maxCount)
+            {
+                return 50;
+            }
+            else if (count < 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return count;
+            }
+        }
+    }
+}
